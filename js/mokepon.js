3ddc7c40b63f6bd99d959963e221bce09dae1a58
PetroGucci <mauriciojvr0@gmail.com>
2023-02-25 00:35:44 -0400

    manipulacion del DOM con iteradores

--- js/mokepon.js
+++ js/mokepon.js
@@ -6,6 +6,7 @@ const botonMascotaJugador=document.getElementById('boton-mascota')
 const botonFuego=document.getElementById('boton-fuego')
 const botonAgua=document.getElementById('boton-agua')
 const botonTierra=document.getElementById('boton-tierra')
+sectionReiniciar.style.display='none'
 const botonReiniciar=document.getElementById('boton-reiniciar')
 
 const sectionSeleccionarMascota=document.getElementById('seleccionar-mascota')
@@ -23,10 +24,12 @@ const spanVidasEnemigo=document.getElementById('vidas-enemigo')
 const sectionMensajes=document.getElementById('resultado')
 const ataquesDelJugador=document.getElementById('ataques-jugador')
 const ataquesDelEnemigo=document.getElementById('ataques-enemigo')
+const contenedorTarjetas=document.getElementById('contenedorTarjetas')
 
 let mokepones=[]
 let ataqueJugador
 let ataqueEnemigo
+let opcionDeMokepones
 let vidasJugador=3
 let vidasEnemigo=3
 
@@ -67,10 +70,23 @@ ratigueya.ataques.push(
     {nombre: '🌱', id:'boton-tierra'},
 )
 
+mokepones.push(hipodoge,capipepo,ratigueya)
+
 function iniciarJuego(){
     
     sectionSeleccionarAtaque.style.display='none'
-    sectionReiniciar.style.display='none'
+
+    mokepones.forEach((mokepon)=>{
+        opcionDeMokepones=`
+        <input type="radio" name="mascota" id=${mokepon.nombre}/>
+                <label class="tarjeta-de-mokepon" for=${mokepon.nombre}>
+                    <p>${mokepon.nombre}</p>
+                    <img src=${mokepon.foto} alt=${mokepon.nombre}>
+                </label>
+        `
+        contenedorTarjetas.innerHTML+=opcionDeMokepones
+    })
+
     botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
     botonFuego.addEventListener('click',ataqueFuego)
     botonAgua.addEventListener('click',ataqueAgua)
